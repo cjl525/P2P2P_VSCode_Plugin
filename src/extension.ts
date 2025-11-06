43ddb8e7922a572c7009a6e3e049f7bb714ebd40
cjl525 <149107519+cjl525@users.noreply.github.com>
2025-11-05 22:04:43 -0700

    FINAL WITHOUT OUT FILES

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,13 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('P2P2P-VSCode activated!');
+  console.log('P2P2P-VSCode activated!');
 
-	let disposable = vscode.commands.registerCommand('p2p2p.generateDiagram', () => {
-		vscode.window.showInformationMessage('P2P2P Diagram Generator Running!');
-	});
+  const disposable = vscode.commands.registerCommand('p2p2p.generateDiagram', () => {
+    vscode.window.showInformationMessage('P2P2P Diagram Generator Running!');
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
